f55842fc4294a16750786771cfac05ccc9370782
syakillasaals <syakillasalsa7@gmail.com>
2025-05-20 15:26:30 +0700

    progress1

---
+++ _Partial/Copyright.php
@@ -0,0 +1,13 @@
+<footer id="footer" class="footer landing_background">
+    <div class="copyright text-white">
+        &copy; Copyright <strong><span><?php echo "$title_page"; ?></span></strong>. All Rights Reserved 2025
+    </div>
+    <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+        <a href="index.php?Page=Version" class="text-light">Kopkai 1.2</a> 
+        Designed by <a href="https://parasilva.site" class="text-light">Kereta Api Indonesia</a>
+    </div>
+</footer>
